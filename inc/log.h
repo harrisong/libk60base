054d7e39ec679a0d3a7701d8be38e28af0b8fedd
Ming Ming <mint2648@gmail.com>
2014-04-16 23:21:27 +0800

    Improve log message

--- inc/log.h
+++ inc/log.h
@@ -9,12 +9,12 @@
 
 #include <stdio.h>
 
-#define LOG_E(fmt, ...) printf("E:" fmt "\n", ##__VA_ARGS__)
-#define LOG_W(fmt, ...) printf("W:" fmt "\n", ##__VA_ARGS__)
-#define LOG_I(fmt, ...) printf("I:" fmt "\n", ##__VA_ARGS__)
+#define LOG_E(fmt, ...) printf("E " fmt "\n", ##__VA_ARGS__)
+#define LOG_W(fmt, ...) printf("W " fmt "\n", ##__VA_ARGS__)
+#define LOG_I(fmt, ...) printf("I " fmt "\n", ##__VA_ARGS__)
 #ifdef DEBUG
-	#define LOG_D(fmt, ...) printf("D:" fmt "\n", ##__VA_ARGS__)
-	#define LOG_V(fmt, ...) printf("V:" fmt "\n", ##__VA_ARGS__)
+	#define LOG_D(fmt, ...) printf("D " fmt "\n", ##__VA_ARGS__)
+	#define LOG_V(fmt, ...) printf("V " fmt "\n", ##__VA_ARGS__)
 #else
 	#define LOG_D(fmt, ...)
 	#define LOG_V(fmt, ...)
